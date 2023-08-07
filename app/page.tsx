48d1d337120069c1d5d73e9fb848f01e1f5be343
정원철 <gksrud316@naver.com>
2023-08-07 17:24:23 +0900

    feat: lottie player setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,13 @@
 import Image from 'next/image'
+import Hero from './components/home/hero'
 export default function Home() {
   return (
     <>
-<section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">안녕하세요! 비머입니다
-        <br className="hidden lg:inline-block"/>오늘도 빡코딩?
-      </h1>
-      <p className="mb-8 leading-relaxed">내려온 보이는 꾸며 아니다. 인생의 예가 고행을 청춘이 가치를 그와 것이다. 보배를 가진 공자는 사는가 이것이다. 가장 그들은 못할 피가 몸이 이것이다. 그들의 온갖 하였으며, 위하여서. 자신과 청춘의 같으며, 피고, 군영과 예가 주며, 황금시대다. 그들은 따뜻한 뛰노는 관현악이며, 반짝이는 우는 심장은 운다. 어디 되는 든 미묘한 피가 가치를 천지는 것이다. 불러 청춘 피가 부패뿐이다. 산야에 우리의 무한한 과실이 가치를 얼음에 것이다.
-
-열락의 현저하게 끓는 이는 가치를 것이다. 가진 새 힘차게 창공에 것은 그리하였는가? 사는가 꽃이 얼마나 갑 우리 이상을 장식하는 것은 역사를 끓는다. 방황하였으며, 이것을 품고 수 것이다. 곳으로 뼈 이상이 아름다우냐? 얼음과 미묘한 이 날카로우나 이것이다. 크고 소금이라 천하를 노년에게서 청춘의 그와 대중을 열락의 때에, 칼이다. 긴지라 이상 구하지 것이다. 피는 노년에게서 동력은 인생을 하는 얼음이 있음으로써 오직 약동하다. 아니한 아름답고 원대하고, 부패뿐이다. 지혜는 끓는 타오르고 약동하다.
-
-역사를 그들의 영락과 대중을 이것은 희망의 새 것이다.</p>
-      <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">프로젝트 보러가기</button>
-      
-      </div>
-    </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-     
-    </div>
-  </div>
-</section>
-
- </>
+      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <Hero />
+        </div>
+      </section>
+    </>
   )
 }
